ca829da0a09bc128d9b3f342d2a6c5465c5f9d59
roshandarekar <roshandarekar@gmail.com>
2011-02-23 19:35:28 +0530

    new bind function

--- public/javascript/controllers/contact.js
+++ public/javascript/controllers/contact.js
@@ -1,55 +1,48 @@
-ContactController = function(app) { with (app) {
+ContactController = function(app) {with (app) {
 
     app.use("Template" , 'html');
     app.use("JSON");
-	//====================================BEFORE LOADING============================
+//====================================BEFORE LOADING============================
     app.before(/^#\/contact-/, function(context) {
             context.log("inside contact");
-			$("#main-content").html('');
-			$("#sidebar-content").html('');
-			$("#content-extra").html('');
-			// make sure the menu remains all the time
-		  	context .render('views/contact-menu.html')
-		  	.replace("#section-menu")
+                    $("#main-content").html('');
+                    $("#sidebar-content").html('');
+                    $("#content-extra").html('');
+            context .render('views/contact-menu.html')
+                    .replace("#section-menu")
             context .render('views/Pager.html')
-			.replace("#sidebar-content")
-            .then(function(contact_html) {
-				$("#section-menu").find("a.contact-add").click(function() {
-                    context.trigger("contact-form");
-                    return false;
-					})
-		  		})
-            });
+                    .replace("#sidebar-content")
+                    .then(function(contact_html) {
+                            $("#section-menu").find("a.contact-add").click(function() {
+                                context.trigger("contact-form");
+                                return false;
+                            })
+                    })
+    });
+//=====================================BIND FUNCTION============================
 
-	bind("contact-delete", function(ev, id) {
-
-		var context = this;
-
+//----------------------------------CONTACT DELETE BIND-------------------------
+        bind("contact-delete", function(ev, id) {
+                var context = this;
 		context.log("comin ghere");
 		$.ajax( {
-
-			url : "/api/Contact/" + id,
-			dataType : "JSON",
-			contentType: "application/json",
-			type : "DELETE",
-			data : {},
-			success: function(json) {
-
-				context.log(json);
-				alert("Contact " + json['data']['name'] + " has been deleted");
-
-                //$("#row_" + id).parents("tr:first").hide();
-                $("#row_" + json['data']['id']).parents("tr:first").hide();
-
-			}
+                    url : "/api/Contact/" + id,
+                    dataType : "JSON",
+                    contentType: "application/json",
+                    type : "DELETE",
+                    data : {},
+                    success: function(json) {
+        		context.log(json);
+                        alert("Contact " + json['data']['name'] + " has been deleted");
+                        $("#row_" + json['data']['id']).parents("tr:first").hide();
+                    }
 		});
 	});
-	//=====================================BIND FUNCTION============================
-	//-------------------------------------PROCESS BIND-----------------------------
+//-------------------------------------PROCESS BIND-----------------------------
     bind('Process',function(){
             var context=this
 
-			//.........................................EDIT.................................
+//.........................................EDIT.................................
             $("#MyTable").find("span.edit").click(function(ev) {
 
                 var id = $(this).attr("id").replace("row_",'');
@@ -59,14 +52,14 @@ ContactController = function(app) { with (app) {
 
                                 });
 
-			//........................................DELETE................................
+//........................................DELETE................................
             $("#MyTable").find("span.delete").click(function() {
 
  				context.trigger("contact-delete", $(this).attr("id").replace("row_",''));
             });
 
 
-			//........................................EXPAND................................
+//........................................EXPAND................................
             $("#MyTable").find("a.expand").click(function() {
 
                 var id = $(this).attr("id").replace("row_",'');
@@ -81,8 +74,7 @@ ContactController = function(app) { with (app) {
                 });
 
     });
-
-	//--------------------------------CONTACE POPULATE BIND-------------------------
+//--------------------------------CONTACE POPULATE BIND-------------------------
     bind("contact-populate", function(ev, data) {
             var context = this;
             context .render('views/contact-list.html')
@@ -93,105 +85,78 @@ ContactController = function(app) { with (app) {
                 .then(function(){
                     context.trigger("Process");
                     $("#MyTable").tablesorter()
-                    .tablesorterPager({ container : $("#pager") , positionFixed: false})
+                    .tablesorterPager({container : $("#pager") , positionFixed: false})
                     })
                 });
             });
-
-	//-------------------------------------CONTACT SAVE BIND------------------------
+            bind("navigate-form", function(ev, data){
+            $("#contact-form")
+                .find("fieldset.step").not("#step1")
+                .hide();
+            $("#contact-form").find("input[name=save]").hide();
+            $("#contact-form").find("a.nav-step").click(function(ev){
+                var stp = $(this).attr("id").replace("nav-",'');
+                $("#contact-form")
+                    .find("fieldset.step")
+                    .hide();
+                    $("#contact-form")
+                    .find("fieldset#" + stp).show();
+                });
+            $("#nav-step4").click(function(ev){
+                $("#contact-form").find("input[name=save]").show();
+            });
+            $(".form-steps").find("input[name=save]").click(function() {
+                $("#contact-form").find("div.box").show();
+            })
+        });
+//-------------------------------------CONTACT SAVE BIND------------------------
     bind('contact-form',function( ev, id){
+            var context  = this;
+            var data ;
+            if (id ){
 
-		var context  = this;
-		var data ;
-		if (id ){
+                console.log(id);
+                $.getJSON("/api/Contact/" + id, function(json){
+                    data = json;
+                })
+            };
+            context.load("/api/Contact/" + id )
+                    .then(function( json ) {
+                        context .render("views/contact-details.html", json )
+                                .then(function(html) {
+                                    console.log(html);
+                                    $.facebox(html);
+                                })
+                                .then( function(html) {
+                                    $("#contact-form" ).find("input.datepicker").datepicker( {altFormat: 'yy-mm-dd' ,dateFormat : 'dd-mm-yy'});
+                                    $("#contact-form").validate({
+                                        messages: {
+                                                    name: "Enter Name",
+                                                    title: "Enter Title",
+                                                    company_id: "Enter Company Id",
+                                                    is_human: "Fill It",
+                                                    user_id: "Enter Id",
+                                                    business_phone: "Enter No.",
+                                                    email: "Enter email id ",
+                                                    business_city: "Enter Bussiness city",
+                                                    web_page: "Enter web page"
 
-		console.log(id);
-			$.getJSON("/api/Contact/" + id, function(json){
-				data = json;
-       	     })
-		};
+                                                },
+                                        errorContainer: "#MSGBOX",
+                                        errorLabelContainer: "#MSGBOX ul",
+                                        wrapper: "li"
+                                    });
+                                    context.trigger("navigate-form");
+                });//end of bind function
+            });
+        });
 
-       context .render("views/contact-details.html", data )
-       	.then(function(html) {
-
-			console.log(html);
-           	$.facebox(html);
-       	}).then( function(html) {
-
-       		$("#contact-form" )
-       			.find("input.datepicker")
-       			.datepicker( { altFormat: 'yy-mm-dd' ,
-       							dateFormat : 'dd-mm-yy'});
- //========================validation part==========================
-               $.validator.setDefaults({
-  	                     submitHandler: function() { alert("submitted!"); }
-                          });
-
-
-	$("#contact-form").validate({
-		               rules: {
-                                    name: "required",
-			            title: "required",
-			            company_id: "required",
-		                    is_human: "required",
-                                    user_id: "required",
-                                    business_phone: "required",
-                                    email: "required",
-                                    business_city: "required",
-                                    web_page: "required"
-                              },
-		              messages: {
-                                    name: "Enter Name",
-			            title: "Enter Title",
-			            company_id: "Enter Company Id",
-		                    is_human: "Fill It",
-                                    user_id: "Enter Id",
-                                    business_phone: "Enter No.",
-                                    email: "Enter email id ",
-                                    business_city: "Enter Bussiness city",
-                                    web_page: "Enter web page"
-
-		              },
-                              errorContainer: "#MSGBOX",
-                              errorLabelContainer: "#MSGBOX ul",
-                              wrapper: "li"
-                        });
-
-        		 $("#contact-form")
-                                .find("input[name=save]").hide()
-                                .find("fieldset.step").not("#step1")
-                                .hide();
-                         $("#contact-form").find("a.nav-step").click(function(ev){
-                             $("#contact-form").find("input[name=save]").hide()
-                                var stp = $(this).attr("id").replace("nav-",'');
-                                $("#contact-form")
-                                    .find("fieldset.step")
-                                    .hide();
-                                $("#contact-form")
-                                    .find("fieldset#" + stp).show();
-                                //return false;
-                         });
-                         $("#nav-step4").click(function(ev){
-                           $("#contact-form").find("input[name=save]").show();
-                         });
-                         $(".form-steps").find("input[name=save]").click(function() {
-                            $("#contact-form").find("div.box").show();
-                                //alert("submit this form");
-                                //return false;
-                         })
-
-    });
-
-
-
-    });
-
-	//=====================================AFTER LOADING============================
+//=====================================AFTER LOADING============================
     app.get('#/contact-list', function(context) {
             context.redirect("#/contact-all");
             });
 
-	//------------------------------------ALL CONTACT-------------------------------
+//------------------------------------ALL CONTACT-------------------------------
     app.get('#/contact-all', function(context) {
             context .load("/api/Contact")
             .then(function(json) {
@@ -200,7 +165,7 @@ ContactController = function(app) { with (app) {
 
    });
 
-	//--------------------------------------COMPANY CONTACT-------------------------
+//--------------------------------------COMPANY CONTACT-------------------------
     app.get('#/contact-company', function(context) {
 			context .load("api/Contact.json")
 			.then(function(json) {
@@ -209,7 +174,7 @@ ContactController = function(app) { with (app) {
 				});
             });
 
-	//--------------------------------------PERSON CONTACT--------------------------
+//--------------------------------------PERSON CONTACT--------------------------
     app.get('#/contact-person', function(context) {
             context .load("http://192.168.2.4:5000/api/Contact")
             .then(function(json) {
@@ -217,7 +182,7 @@ ContactController = function(app) { with (app) {
                 });
             });
 
-	//--------------------------------------------POST------------------------------
+//--------------------------------------------POST------------------------------
     app.post("#/contact-add", function(context) {
 			var form = context.params.toHash();
 			alert("coming");
@@ -228,7 +193,7 @@ ContactController = function(app) { with (app) {
 				dataType : "JSON",
 				contentType: "application/json",
 				type : "POST",
-				data : { "Contact" : form},
+				data : {"Contact" : form},
 				success: function(json) {
 
 					context.log(json);
