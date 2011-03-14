167acdbd3b1afde7a02167e4e46be9015fe8232f
suyog <suyog_s87@yahoo.co.in>
2011-03-14 18:50:13 +0530

    profile page with all details

--- public/javascript/controllers/profile.js
+++ public/javascript/controllers/profile.js
@@ -9,116 +9,30 @@ ProfileController = function(app) {with (app) {
                 $("#main-content").html('');
                 $("#sidebar-content").html('');
                 $("#content-extra").html('');
-                context .render('views/profile-menu.html').replace("#section-menu")
-        });
-//====================================BIND FUNCTION=============================
-
-/*/-----------------------------------PROCESS BIND-------------------------------
-        bind('Process',function(){
-                var context=this
-
-//......................................EDIT....................................
-                $("#MyTable").find("span.edit").click(function(ev) {
-                    var id = $(this).attr("id").replace("row_",'');
-                    if(confirm('Are you sure')) {
-                        context.trigger("contact-form", id);
-                    }
-                });
-
-//.....................................DELETE...................................
-                $("#MyTable").find("span.delete").click(function() {
-                    var id= $(this).attr("id").replace("row_",'');
-                    var context = this;
-                    if(confirm('Are you sure')) {
-                        var callback = function(json) {
-                            context.log(json);
-                            $("#row_" + json['data']['id']).parents("tr:first").hide();
-                        }
-                        context.remove("Contact", id, callback) ;
-                    }
-                });
-
-//......................................EXPAND..................................
-                $("#MyTable").find("a.expand").click(function() {
-                    var id = $(this).attr("id").replace("row_",'');
-                	context .load("/api/Contact/"+id)
-                                .then(function(json){
-                                    context .jemplate('Expand.html', json['data'], null, this)
-                                })
-                                .then(function(content){
-                                    $.facebox( content );
-                                })
-               })
-
         });
 
-//-----------------------------------NAVIGATE FORM------------------------------
-        bind("navigate-form", function(ev, data){
-                $("#contact-form")
-                    .find("fieldset.step").not("#step1")
-                    .hide();
-                $("#contact-form").find("a.nav-step").click(function(ev){
-                    var stp = $(this).attr("id").replace("nav-",'');
-                    $("#contact-form")
-                        .find("fieldset.step")
-                        .hide();
-                    $("#contact-form")
-                        .find("fieldset#" + stp).show();
-                });
-        });
 
-//----------------------------------CONTACT FORM BIND---------------------------
-        bind('contact-form',function( ev, id){
-                var context  = this;
-                var data;
-                context .load('null.html')
-                        .then(function(html){
-                            context .jemplate('contact-details.html',{
-                        	data : context.fetch("Contact", id),
-                        	contacts : context.look_for("Contact", "company")
-                            } ,null,this);
-                        })
-                        .then(function( html ) {
-                            $.facebox(html);
-                        })
-                        .then( function(html) {
-                            $("#contact-form" ).find("input.datepicker").datepicker( {altFormat: 'yy-mm-dd' ,dateFormat : 'dd-mm-yy'});
-                            $("#contact-form").validate({
-                                messages: {
-                                    name: "Enter Name",
-                                    title: "Enter Profession Title",
-                                    is_human: "Fill It",
-                                    primary_phone: "Enter No.",
-                                    email: "Enter email id ",
-                                    business_city: "Enter Bussiness city",
-                                    web_page: "Enter web page"
-                                },
-                                errorContainer: "#MSGBOX",
-                                errorLabelContainer: "#MSGBOX ul",
-                                wrapper: "li"
-                            });
-                            context.trigger("navigate-form");
-                        });
-        });
-*/
 //==================================AFTER LOADING===============================
 //------------------------------------LOADING PROFILE--------------------------
         app.get('#/profile-list', function(context) {
-                context .render("views/profile-view.html").replace("#main-content")
-                //context .render('views/profile-menu.html').replace("#section-menu")
-        });
-
-//----------------------------------------POST----------------------------------
-        app.post("#/profile-edit", function(context) {
-                var form = context.params.toHash();
-		alert("coming");
-                console.log(form);
-                var callback= function(json) {
-                    context.log(json);
-                    //$("#Save").trigger('close.facebox');
-                    context.render('views/profile.html').replace('#section-menu')
-                }
-                context.save("Profile", form, callback);
-        });
-
+              context .render("views/profile-view.html").replace("#main-content")
+                      .then (function(){
+                           $("#adv_detail").hide();
+                       });
+             context .render('views/profile-menu.html').replace("#section-menu")
+                     .then (function(){
+                           $("#shown").find("a.expand").click(function(ev) {
+                               alert("shows personal details");
+                               $("#adv_detail").show();
+                           })
+						   $("#shown").find("a.change_password").click(function(ev) {
+                              context .render('views/change_password.html')
+                                      .replace("#sidebar-content");
+							})
+							$("#shown").find("a.back_up").click(function(ev) {
+                              context .render('views/back_up.html')
+                                      .replace("#sidebar-content")
+							})
+						})
+      });
 }}
